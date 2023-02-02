f329263ee2ca99cbd9efa5089c2576e758a51b40
Aleksandra Kukla <aleksandra.kukla@yahoo.com>
2023-02-02 23:34:22 +0100

    Implement doAccumulate function for NoopCalculator

--- app/module/Statistics/src/Calculator/NoopCalculator.php
+++ app/module/Statistics/src/Calculator/NoopCalculator.php
@@ -9,12 +9,34 @@ use Statistics\Dto\StatisticsTo;
 
 class NoopCalculator extends AbstractCalculator
 {
+
+    protected const UNITS = 'posts per user';
+
+    /**
+     * @var array
+     */
+    private $postTotals = [];
+
+    /**
+     * @var array
+     */
+    private $userList = [];
+
     /**
      * @inheritDoc
      */
     protected function doAccumulate(SocialPostTo $postTo): void
     {
-        // Noops!
+        $key = $postTo->getDate()->format('M, Y');
+
+        $this->postTotals[$key] = ($this->postTotals[$key] ?? 0) + 1;
+
+        if (!isset($this->userList[$key])){
+            $this->userList[$key] = [];
+        }
+        if (!in_array($postTo->getAuthorId(), $this->userList[$key])){
+            $this->userList[$key][] = $postTo->getAuthorId();
+        }
     }
 
     /**
