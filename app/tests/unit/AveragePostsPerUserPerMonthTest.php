7ac21c670e9fa9dfa0cb6678880a288363db7b81
Aleksandra Kukla <aleksandra.kukla@yahoo.com>
2023-02-04 13:08:09 +0100

    Refactor: extract setUpBeforeClass method, give proper names to variables

--- app/tests/unit/AveragePostsPerUserPerMonthTest.php
+++ app/tests/unit/AveragePostsPerUserPerMonthTest.php
@@ -11,6 +11,7 @@ use Statistics\Dto\ParamsTo;
 use Statistics\Enum\StatsEnum;
 use Statistics\Service\Factory\StatisticsServiceFactory;
 use SocialPost\Hydrator\FictionalPostHydrator;
+
 /**
  * Class ATestAveragePostsPerUserPerMonth
  *
@@ -18,38 +19,57 @@ use SocialPost\Hydrator\FictionalPostHydrator;
  */
 class AveragePostsPerUserPerMonthTest extends TestCase
 {
-    /**
-     * @test
-     */
-    public function testStatsForOneMonth(): void
+    private $params;
+
+    protected function setUpBeforeClass(): void
     {
         $startDate = DateTime::createFromFormat('Y-m-d H:i:s', '2018-08-01 00:00:00');
         $endDate   = DateTime::createFromFormat('Y-m-d H:i:s', '2018-08-31 23:59:59');
-        $params = [
+        $this->params = [
             (new ParamsTo())
                 ->setStatName(StatsEnum::AVERAGE_POSTS_NUMBER_PER_USER_PER_MONTH)
                 ->setStartDate($startDate)
                 ->setEndDate($endDate)
         ];
+    }
 
-        $posts = $this->fetchPosts();
+    /**
+     * @test
+     */
+    public function testStatsForOneMonth(): void
+    {
+        $postsJson = file_get_contents('./tests/data/social-posts-response.json');
+        $responseData = json_decode($postsJson, true);
+
+        $posts = $this->fetchPosts($responseData['data']['posts']);
         $statsService = StatisticsServiceFactory::create();
-        $stats = $statsService->calculateStats($posts, $params);
+        $stats = $statsService->calculateStats($posts, $this->params);
 
-        $noopStats = $stats->getChildren();
-        $allMonthsStats = $noopStats[0]->getChildren();
+        $averageStats = $stats->getChildren();
+        $allMonthsStats = $averageStats[0]->getChildren();
         $monthStats = $allMonthsStats[0];
         $averagePostsPerUserPerMonth = $monthStats->getValue();
 
         $this->assertEquals(1, $averagePostsPerUserPerMonth);
     }
 
-    private function fetchPosts() : Traversable {
-        $hydrator = new FictionalPostHydrator();
+    /**
+     * @test
+     */
+    public function testForNoPosts(): void
+    {
+        $posts = $this->fetchPosts([]);
+        $statsService = StatisticsServiceFactory::create();
+        $stats = $statsService->calculateStats($posts, $this->params);
 
-        $postsJson = file_get_contents('./tests/data/social-posts-response.json');
-        $responseData = json_decode($postsJson, true);
-        $posts = $responseData['data']['posts'];
+        $averageStats = $stats->getChildren();
+        $allMonthsStats = $averageStats[0]->getChildren();
+
+        $this->assertEmpty($allMonthsStats);
+    }
+
+    private function fetchPosts($posts) : Traversable {
+        $hydrator = new FictionalPostHydrator();
 
         foreach ($posts as $postData) {
                 yield $hydrator->hydrate($postData);
